3bec50d18c5c3a40f229fa17faf128d3016b6ea0
javigvr <jsanchezgav@gmail.com>
2025-01-13 11:39:50 +0000

    generador de excusas2.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["El perro", "La abuela", "El cartero", "El pajarito"];
+  let accion = ["comio", "piso", "cago", "rompio"];
+  let que = ["mis deberes", "mi telefono", "el coche"];
+  let cuando = [
+    "antes de clase",
+    "cuando me hiba a dormir",
+    "cuando hacia deporte",
+    "durante la comida",
+    "cuando estaba jugando"
+  ];
+  //variables aleatorias
+  let random1 = Math.floor(Math.random() * quien.length);
+  let random2 = Math.floor(Math.random() * accion.length);
+  let random3 = Math.floor(Math.random() * que.length);
+  let random4 = Math.floor(Math.random() * cuando.length);
+  //La respuesta
+  document.getElementById("respuesta").innerHTML =
+    quien[random1] +
+    " " +
+    accion[random2] +
+    " " +
+    que[random3] +
+    " " +
+    cuando[random4];
+
+
 };
